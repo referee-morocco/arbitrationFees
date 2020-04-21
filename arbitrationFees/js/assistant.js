8160e4732fd83dfac3511692108435470f1b9ab1
referee-morocco <61735945+referee-morocco@users.noreply.github.com>
2020-04-21 22:19:59 +0000

    Update assistant.js

--- arbitrationFees/js/assistant.js
+++ arbitrationFees/js/assistant.js
@@ -1,22 +1,17 @@
-function validation(){
-    var distance = document.getElementById('distance').value;
-    var frais = Number(document.getElementById('frais').value);
-    var fraisTotal;
-    
-    if (distance==''){
-        alert("veuillez entrer la distance");
-        distance=0;
-    }else if(frais==0){
-        alert("veuillez choisir la division")
-    }else if(frais>=1300 && distance<300){
-        fraisTotal=distance*2+frais;
-    }else if(frais>=1300 && distance>=300){
-        fraisTotal=distance*2+1200+frais;
-    }else if(frais<1300 && distance<300){
-        fraisTotal=distance*2+frais;
-    }else if(frais<1300 && distance>=300){
-        fraisTotal=distance*2+500+frais;
-    }
-    if(frais > 0 && distance != '')
-        alert(fraisTotal + " DH");
+function validation() {
+  var distance = document.getElementById('distance').value;
+  var frais = Number(document.getElementById('frais').value);
+  var fraisTotal;
+
+  if (frais >= 1300 && distance < 300) {
+    fraisTotal = distance * 2 + frais;
+  } else if (frais >= 1300 && distance >= 300) {
+    fraisTotal = distance * 2 + 1200 + frais;
+  } else if (frais < 1300 && distance < 300) {
+    fraisTotal = distance * 2 + frais;
+  } else if (frais < 1300 && distance >= 300) {
+    fraisTotal = distance * 2 + 500 + frais;
+  }
+  if (frais > 0 && distance != '')
+    alert(fraisTotal + " DH");
 }
